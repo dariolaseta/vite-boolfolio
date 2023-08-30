a3a2e8e50fb8067e7f4eb81db673f457bd78f8e3
Oxidra <dantelezzo@gmail.com>
2023-08-30 14:42:12 +0200

    got axios response in console log

---
+++ src/components/Project.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: "Project",
+    data(){
+        return {
+            projectUrl: "http://localhost:81/api/projects",
+            projects: "",
+        }
+    },
+    mounted(){
+        axios.get(this.projectUrl)
+        .then(response=>{
+            this.projects = response.data.projects;
+            console.log(this.projects);
+        })
+    }
+}
+</script>
+
+<style lang="">
+    
+</style>
