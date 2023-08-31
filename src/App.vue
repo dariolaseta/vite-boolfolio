1d724e831a58af00c17c403da73d048f75c08333
Oxidra <dantelezzo@gmail.com>
2023-08-31 15:07:04 +0200

    added AppHeader & AppMain

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <script>
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
 
+export default{
+  name: "App",
+  
+  components:{
+    AppHeader,
+    AppMain
+  }
+}
 </script>
 
 <template>
-
+  <AppHeader />
+  <AppMain />
+<router-view></router-view>
 </template>
 
-<style>
+<style lang="scss">
 
 </style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,15 @@
+<template>
+    <main>
+        <h1>Main</h1>
+    </main>
+</template>
+
+<script>
+export default {
+    name: "AppMain"
+}
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
