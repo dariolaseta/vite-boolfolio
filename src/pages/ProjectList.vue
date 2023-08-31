1389bcf7d068ab53b0c90fd204519b829f8ee468
Oxidra <dantelezzo@gmail.com>
2023-08-31 15:31:05 +0200

    show project to page

--- src/pages/ProjectList.vue
+++ src/pages/ProjectList.vue
@@ -1,15 +1,36 @@
 <template>
     <div>
-        <h1>Project List</h1>
+        <h1>Projects</h1>
+    </div>
+
+    <div v-for="project in projects">
+        <div class="project">
+            {{ project.title }}
+        </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-    name: "ProjectList"
+    name: "ProjectList",
+    data(){
+        return {
+            projectUrl: "http://localhost:81/api/projects",
+            projects: "",
+        }
+    },
+    mounted(){
+        axios.get(this.projectUrl)
+        .then(response=>{
+            this.projects = response.data.projects;
+            console.log(this.projects);
+        })
+    }
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="">
     
 </style>
