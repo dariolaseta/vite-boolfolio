d8a3e85ad2d75696bdca9b2db9aa4a23c9206434
Oxidra <dantelezzo@gmail.com>
2023-08-31 15:20:22 +0200

    fixed typo

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 import AppHome from './pages/AppHome.vue';
-import PostList from './pages/ProjectList.vue';
+import ProjectList from './pages/ProjectList.vue';
 
 const router = createRouter({
     history: createWebHistory(),
